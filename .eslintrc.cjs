b04f5413b5c49d04f88d273497a172370fbaaf35
kr-younghoon <sullanta0802@gmail.com>
2024-04-01 15:22:40 +0900

    [#1]Feat: prettier, eslint 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'react-app',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -12,6 +13,12 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'no-var': 'error',
+    'no-multiple-empty-lines': 'error',
+    "no-console": ["warn", { "allow": ["warn", "error", "info"] }],
+    "eqeqeq": "error",
+    "no-unused-vars": "warn",
+    "no-undef": "warn",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
