549e2c6122154e8b55263b921e231f6ce7c84970
kyeongb-bin <kyeongb0315@gmail.com>
2024-04-05 00:44:08 +0900

    [#5]Stlye: hover 효과 수정

--- src/component/LoginButton.jsx
+++ src/component/LoginButton.jsx
@@ -2,41 +2,27 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
     width: 320px;
-    height: 55px;
+    height: 45px;
     padding: 10px 25px;
-    border: 2px solid #000;
+    border: 2px solid #5271ff;
     border-radius: 8px;
-    font-family: 'Lato', sans-serif;
     font-weight: 500;
-    background: transparent;
+    font-size: 14px;
+    background-color: #5271ff;
     cursor: pointer;
-    transition: all 0.3s ease;
+    color: #fff;
     position: relative;
-    display: inline-block;
-    transition: all 0.3s ease;
-    overflow: hidden;
-
-    &:after {
-        position: absolute;
-        content: " ";
-        top: 0;
-        left: 0;
-        z-index: -1;
-        width: 100%;
-        height: 100%;
-        transition: all 0.3s ease;
-        -webkit-transform: scale(.1);
-        transform: scale(.1);
-    }
 
     &:hover {
-        color: #fff;
-    }
-
-    &:hover:after {
-        background: #000;
-        -webkit-transform: scale(1);
-        transform: scale(1);
+        box-shadow:
+            -3px -3px 10px 0px #fff9,
+            -2px -2px 2px 0px #fff9,
+            3px 3px 10px 0px #0002,
+            2px 2px 2px 0px #0001,
+            3px -3px 10px 0px #fff9,
+            2px -2px 2px 0px #fff9,
+            -3px 3px 10px 0px #0002,
+            -2px 2px 2px 0px #0001;
     }
 `;
 
