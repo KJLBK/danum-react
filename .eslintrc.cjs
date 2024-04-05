1efe2cd049ea3e2b92821526aff10b0cd68632d0
kr-younghoon <sullanta0802@gmail.com>
2024-04-06 05:08:13 +0900

    [#14]Chore: styled-components에 대한 예외룰 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,7 @@
 module.exports = {
 	root: true,
-	env: { browser: true, es2020: true },
+	env: { node: true, browser: true, es2021: true },
 	extends: [
-		'react-app',
 		'eslint:recommended',
 		'plugin:react/recommended',
 		'plugin:react/jsx-runtime',
@@ -11,12 +10,13 @@ module.exports = {
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-	settings: { react: { version: '18.2' } },
+	settings: { react: { version: 'detect' } },
 	plugins: ['import', 'react', 'react-refresh'],
 	rules: {
 		'no-var': 'error',
 		'no-multiple-empty-lines': 'error',
 		'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+		'import/no-named-as-default': 'off',
 		eqeqeq: 'error',
 		'no-unused-vars': 'warn',
 		'no-undef': 'warn',
