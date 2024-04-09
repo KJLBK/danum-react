508eca70e0a0107f8093ee764e75a81ac9ca4d60
kr-younghoon <sullanta0802@gmail.com>
2024-04-09 23:16:08 +0900

    Chore: 머지를 위한 conflict 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
-import LoginBtn from '../component/LoginBtn';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Home() {
-  const [hello, setHello] = useState('');
+	const [hello, setHello] = useState('')
 
-  useEffect(() => {
-    axios.get('/api/test').then((res) => {
-      setHello(res.data);
-    });
-  });
-  return (
-    <>
-      <h1>danum-frontend12120</h1>
-      <p>Backend Data : {hello}</p>
-    </>
-  );
+	useEffect(() => {
+		axios.get('/api/test').then((res) => {
+			setHello(res.data)
+		})
+	})
+	return (
+		<>
+			<h1>danum-frontend #1</h1>
+			<p>Backend Data : {hello}</p>
+		</>
+	)
 }
