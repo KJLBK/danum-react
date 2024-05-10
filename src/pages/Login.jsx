00222a45679f918d0cd433f4cf5f277818bfa8c5
kr-younghoon <sullanta0802@gmail.com>
2024-05-11 05:02:42 +0900

    Edit: 오타 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useAuth } from '../store/useAuth.jsx'
+import useAuth from '../store/useAuth.jsx'
 import axios from 'axios'
 
 export default function Login() {
