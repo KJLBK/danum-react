6f4ba38d14fe79753df9260dfa3286c2569202fd
kr-younghoon <sullanta0802@gmail.com>
2024-05-30 19:09:01 +0900

    chore: BrowserRouter 코드 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Join from './pages/Join.jsx'
 import Login from './pages/Login.jsx'
@@ -9,18 +9,16 @@ import Write from './pages/Write.jsx'
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="join" element={<Join />} />
-				<Route path="login" element={<Login />} />
-				<Route path="fe-test" element={<Test />} />
-				{/* 게시판 */}
-				<Route path="dev-board" element={<BoardList />} />
-				<Route path="dev-board/:id" element={<BoardDetail />} />
-				<Route path="write" element={<Write />} />
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="join" element={<Join />} />
+			<Route path="login" element={<Login />} />
+			<Route path="fe-test" element={<Test />} />
+			{/* 게시판 */}
+			<Route path="dev-board" element={<BoardList />} />
+			<Route path="dev-board/:id" element={<BoardDetail />} />
+			<Route path="write" element={<Write />} />
+		</Routes>
 	)
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+	<BrowserRouter>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</BrowserRouter>,
 )
