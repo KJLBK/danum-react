be234c5a22366faa8f81eb5d8943dd264c0620ab
kr-younghoon <sullanta0802@gmail.com>
2024-06-09 03:01:40 +0900

    chore: Update config and add env variable setup

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IP 주소를 변수로 정의합니다.
-const API_URL_1 = 'http://43.203.8.51:8080' // Danum(main)
-// const API_URL_2 = 'http://118.32.97.217:8080' // 성준
-// const API_URL_3 = 'http://125.141.52.238:8080' // 승민
-
 // https://vitejs.dev/config/
-export default defineConfig({
-	base: './',
-	plugins: [react()],
-	assetsInclude: ['**/*.jpg'],
-	server: {
-		cors: true,
-		proxy: {
-			'/api': {
-				target: API_URL_1,
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-				// ws: true, // WebSocket 프록시 활성화
+export default ({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return defineConfig({
+		base: './',
+		plugins: [react()],
+		assetsInclude: ['**/*.jpg'],
+		server: {
+			cors: true,
+			proxy: {
+				'/api': {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ''),
+					// ws: true, // WebSocket 프록시 활성화
+				},
 			},
 		},
-	},
-	define: {
-		global: 'window',
-	},
-})
+		define: {
+			global: 'window',
+		},
+	})
+}
