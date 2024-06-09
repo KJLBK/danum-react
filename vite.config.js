6401356718fd41058273ec7da67c646c25843fd0
kyeongb-bin <kyeongb0315@gmail.com>
2024-06-09 15:14:57 +0900

    rebase

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 // https://vitejs.dev/config/
+<<<<<<< HEAD
 export default defineConfig({
 	base: './',
 	plugins: [react()],
@@ -15,10 +16,28 @@ export default defineConfig({
 				target: process.env.VITE_API_BASE_URL,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
+=======
+export default ({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return defineConfig({
+		base: './',
+		plugins: [react()],
+		assetsInclude: ['**/*.jpg'],
+		server: {
+			cors: true,
+			proxy: {
+				'/api': {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ''),
+					// ws: true, // WebSocket 프록시 활성화
+				},
+>>>>>>> cae1d65 (임시)
 			},
 		},
-	},
-	define: {
-		global: 'window',
-	},
-})
+		define: {
+			global: 'window',
+		},
+	})
+}
