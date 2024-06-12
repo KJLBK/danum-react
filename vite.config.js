3bbc68a223a349e2e15fab19dc5e3bdbfc50b5bf
kr-younghoon <sullanta0802@gmail.com>
2024-06-12 15:25:06 +0900

    edit: Check to address(vite.config.js)

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // https://vitejs.dev/config/
-<<<<<<< HEAD
 export default defineConfig({
 	base: './',
 	plugins: [react()],
@@ -14,30 +13,15 @@ export default defineConfig({
 		proxy: {
 			'/api': {
 				target: process.env.VITE_API_BASE_URL,
+				ws: true,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
-=======
-export default ({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '')
-
-	return defineConfig({
-		base: './',
-		plugins: [react()],
-		assetsInclude: ['**/*.jpg'],
-		server: {
-			cors: true,
-			proxy: {
-				'/api': {
-					target: env.VITE_API_URL,
-					changeOrigin: true,
-					rewrite: (path) => path.replace(/^\/api/, ''),
-					// ws: true, // WebSocket 프록시 활성화
-				},
->>>>>>> cae1d65 (임시)
 			},
 		},
-		define: {
-			global: 'window',
-		},
-	})
-}
+	},
+	define: {
+		global: 'window',
+	},
+})
+/* eslint-disable no-console */
+console.log(`  ➜  Address: ${process.env.VITE_API_BASE_URL}`)
